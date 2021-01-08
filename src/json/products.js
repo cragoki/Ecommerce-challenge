bcb2c4d733d72578ab8222d64829a0bbe4b5c53f
Craig Rodger <craigrodger1@hotmail.com>
2021-01-08 13:14:43 +0100

    Adding images and Data
    Adding images and JSON file data, going to try and see how much of an E-commerce site I can get done in 2 hours

---
+++ src/json/products.js
@@ -0,0 +1,66 @@
+const products = 
+[
+    {
+        id: "1",
+        type: "T-shirt",
+        brand: "Nike",
+        price: 12,
+        description: "A white Nike t-shirt",
+        img: "t-shirt-nike.jpg",
+        color: "White"
+    },
+    {
+        id: "2",
+        type: "T-shirt",
+        brand: "Addidas",
+        price: 12,
+        description: "A white Addidas t-shirt",
+        img: "t-shirt-addidas.jpg",
+        color: "White"
+    },
+    {
+        id: "3",
+        type: "Shorts",
+        brand: "Nike",
+        price: 6,
+        description: "Some Nike shorts",
+        img: "shorts-nike.jpg",
+        color: "Black"
+    },
+    {
+        id: "4",
+        type: "Polo Shirt",
+        brand: "Lacoste",
+        price: 18,
+        description: "A white Lacoste T-shirt",
+        img: "polo-lacoste.jpg",
+        color: "White"
+    },
+    {
+        id: "5",
+        type: "Hoodie",
+        brand: "QuickSilver",
+        price: 30,
+        description: "A Grey Quicksilver Hoodie",
+        img: "hoodie-qs.jpg",
+        color: "Grey"
+    },
+    {
+        id: "6",
+        type: "T-shirt",
+        brand: "QuickSilver",
+        price: 9,
+        description: "A white QS T-shirt",
+        img: "t-shirt-qs.jpg",
+        color: "White"
+    },
+    {
+        id: "7",
+        type: "Shoes",
+        brand: "Addidas",
+        price: 40,
+        description: "Some Addidas Shoes",
+        img: "shoes-addidas.jpg",
+        color: "Black"
+    },
+]
